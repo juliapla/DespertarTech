294a93d1f8a19511d6684bb0eb5e4abd5f76cb67
juliapla <127748094+juliapla@users.noreply.github.com>
2023-03-13 09:03:26 -0300

    Add files via upload

---
+++ algoritimo.js
@@ -0,0 +1,15 @@
+var numeroUm = 0; 
+let numeDois = 10;  
+var numeroTres= 20;
+
+//diferenca enre const e ver 
+//diferenca entre ler e ver 
+
+console.log(numeroUm)
+//para que serve o console.log e como usa-localStorage
+
+numeroUm=numeDois;
+console.log(numeroUm)    
+
+numeroUm=numeroTres;
+console.log(numeroUm)
